ec1f366dbf12d5743f55b443ce4b89fab9454c2d
Shyamala Sarathy <105268170+shyamalasarathy@users.noreply.github.com>
2022-06-21 19:32:09 +0530

    Adding test to find the error

--- misaligned.py
+++ misaligned.py
@@ -4,10 +4,16 @@ def print_color_map():
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
+            global index0
+            pair_number = i * 5 + j
+            if (i == 0) and (j == 0):
+                index0 = pair_number
+            print(f'{pair_number} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
-
 
 result = print_color_map()
 assert(result == 25)
 print("All is well (maybe!)\n")
+
+#Indexing error
+assert(index0==1)
