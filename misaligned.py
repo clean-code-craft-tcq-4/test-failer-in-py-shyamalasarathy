5d42d763360e20a89675b382e172797e90d27b3b
Shyamala Sarathy <105268170+shyamalasarathy@users.noreply.github.com>
2022-06-28 17:58:16 +0530

    Update to pass test

--- misaligned.py
+++ misaligned.py
@@ -1,14 +1,14 @@
-
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    Length_Colors = len(minor_colors)
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             global index0
-            pair_number = i * 5 + j
+            pair_number = i * Length_Colors + j + 1
             if (i == 0) and (j == 0):
                 index0 = pair_number
-            print(f'{pair_number} | {major} | {minor}')
+            print(f'{pair_number:<3} | {major:<7} | {minor}')
     return len(major_colors) * len(minor_colors)
 
 result = print_color_map()
